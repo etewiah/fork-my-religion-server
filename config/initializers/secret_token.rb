231b7daefcd47399643d7d8743c6c985ab6fd6e8
Ed Tewiah <etewiah@hotmail.cim>
2013-10-27 00:05:32 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForkMyReligionServer::Application.config.secret_token = 'e5d81971e16c8d1f911a8b4a639bb957a9527fd3d9f99946b4bef14e397aef2d2b0db9d1149a62cde5c2b99943f28a5c92237b3c0bbf3235651220462074fd88'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ForkMyReligionServer::Application.config.session_store :cookie_store, key: '_fork-my-religion-server_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ForkMyReligionServer::Application.config.session_store :active_record_store
